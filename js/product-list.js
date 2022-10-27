42dbf4ff94a8fa8304f883637e0f7737e91ffb56
Martin Rodriguez <neomartinr@gmail.com>
2022-10-26 21:29:25 -0300

    Edit product, setFavorite

--- js/product-list.js
+++ js/product-list.js
@@ -1,37 +1,38 @@
 // Voy a mirar si en el local storage hay productos guardados. Si hay los voy a leer y los voy asignar a mi variable Productos
-const Products =  JSON.parse(localStorage.getItem('products')) || [];
+const Products = JSON.parse(localStorage.getItem('products')) || [];
 const tableBodyHTML = document.getElementById('table-body');
 const addProductForm = document.querySelector('#addProductForm');
-
+let editable;
 
 console.log(tableBodyHTML)
 
 function renderProducts() {
     tableBodyHTML.innerHTML = '';
     Products.forEach((elem, index) => {
-        tableBodyHTML.innerHTML += `<tr>
-                                        <td>
-                                            <img class="table-img" src=${elem.image} />
-                                        </td>
-                                        <td>${elem.name}</td>
-                                        <td>${elem.description}</td>
-                                        <td>$ ${elem.price}</td>
-                                        <td class="">
-                                            ${elem.stock ? `<i class="fa-solid fa-box me-2"></i>` : `` }
-                                            ${elem.jostick ? `<i class="fa-solid fa-gamepad me-2"></i>` : ``}
-                                            ${elem.favorite ? `<i class="fa-solid fa-star"></i>` : `<i class="fa-regular fa-star"></i>`}
-                                        </td>
-                                        <td class="">
-                                            <div class="d-flex">
-                                                <button class="btn btn-danger p-1 me-2" onclick="deleteProduct(${index})">
-                                                    <i class="fa-solid fa-trash"></i>
-                                                </button>
-                                                <button class="btn btn-success p-1" onclick="editProduct(${index})">
-                                                    <i class="fa-solid fa-pen"></i>
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>`
+        tableBodyHTML.innerHTML += `
+            <tr>
+                <td>
+                    <img class="table-img" src=${elem.image} />
+                </td>
+                <td>${elem.name}</td>
+                <td>${elem.description}</td>
+                <td>$ ${elem.price}</td>
+                <td class="">
+                    <i class="fa-solid fa-box me-2 ${elem.stock ? `text-primary` : `text-muted`} "></i>
+                    ${elem.jostick ? `<i class="fa-solid fa-gamepad me-2"></i>` : ``}
+                    <span onclick="setFavorite(${index})">${elem.favorite ? `<i class="fa-solid fa-star text-yellow"></i>` : `<i class="fa-regular fa-star"></i>`} </span>
+                </td>
+                <td class="">
+                    <div class="d-flex">
+                        <button class="btn btn-danger p-1 me-2" onclick="deleteProduct(${index})">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                        <button class="btn btn-success p-1" onclick="editProduct(${index})">
+                            <i class="fa-solid fa-pen"></i>
+                        </button>
+                    </div>
+                </td>
+            </tr>`
     });
 }
 
@@ -40,7 +41,7 @@ renderProducts();
 
 addProductForm.addEventListener('submit', (evt) => {
 
-    if(addProductForm.checkValidity() === false) {
+    if (addProductForm.checkValidity() === false) {
         // Freno la ejecución de la funcion de callback del evento submit
         return;
     }
@@ -65,25 +66,32 @@ addProductForm.addEventListener('submit', (evt) => {
     newProduct.price = parseInt(newProduct.price)
 
     console.log(newProduct)
-    
+
     newProduct.jostick = !!newProduct.jostick;
     newProduct.stock = !!newProduct.stock;
 
-    //Añadí un nuevo producto a mi array de productos
-    if(addProductForm.dataset.edit) {
-        Products[addProductForm.dataset.edit] = newProduct;
+    if(editable) {
+        Products[editable] = newProduct;
     } else {
         Products.push(newProduct)
     }
 
-    delete addProductForm.dataset.edit;
+    editable = undefined;
+
+    //Añadí un nuevo producto a mi array de productos
+    // if (addProductForm.dataset.edit) {
+    //     Products[addProductForm.dataset.edit] = newProduct;
+    // } else {
+    //     Products.push(newProduct)
+    // }
+
+    // delete addProductForm.dataset.edit;
 
 
     console.log(Products)
     //Guardamos el array de productos modificado
     localStorage.setItem('products', JSON.stringify(Products))
 
-    localStorage.removeItem('saludo')
 
     // Pintamos nuevamente la tabla para que reflejen los cambios
     renderProducts();
@@ -95,26 +103,61 @@ addProductForm.addEventListener('submit', (evt) => {
     const nameInput = document.getElementById('name')
     // le hago focus (pongo el curso en ese input)
     nameInput.focus();
-    
+
 
 })
 
 function deleteProduct(idx) {
     console.log(`delete`)
     Products.splice(idx, 1);
+    localStorage.setItem('products', JSON.stringify(Products))
     renderProducts();
 }
 
+// Function @param indice del elemento a editar
 function editProduct(idx) {
-    const prod = Products[idx]
-    Object.keys(prod).forEach(key => {
-        // if(`checked` in addProductForm.elements[key]) {
-            console.log(key, !!prod[key])
-            addProductForm.elements[key].checked = !!prod[key]
-        // }
-       
-        addProductForm.elements[key].value = Products[idx][key]
-    })
-    addProductForm.setAttribute('data-edit', idx);
+    //Obtengo del array productos el producto completo (objeto)
+    const productToEdit = Products[idx]
+
+    //Obtengo los inputs elementos de mi formulario
+    const formEl = addProductForm.elements
+
+    //Empiezo a rellenar los valor de los inputs con los valores de las propiedas del producto a editar
+    formEl.name.value = productToEdit.name;
+    formEl.description.value = productToEdit.description;
+    formEl.price.value = productToEdit.price;
+    formEl.image.value =  productToEdit.image;
+
+    formEl.stock.checked = productToEdit.stock;
+    formEl.jostick.checked = productToEdit.checked;
+
+
+    // addProductForm.setAttribute('edit', idx)
+    editable = idx;
+
+
+
+    // const prod = Products[idx]
+    // Object.keys(prod).forEach(key => {
+    //     // if(`checked` in addProductForm.elements[key]) {
+    //     console.log(key, !!prod[key])
+    //     addProductForm.elements[key].checked = !!prod[key]
+    //     // }
+
+    //     addProductForm.elements[key].value = Products[idx][key]
+    // })
+    // addProductForm.setAttribute('data-edit', idx);
+
+    
 }
 
+function setFavorite(indice) {
+    console.log(`adsadas`)
+    Products.forEach((prod) => {
+        prod.favorite = false;
+    })
+    Products[indice].favorite = true;
+    console.log(Products);
+    renderProducts();
+    localStorage.setItem('products', JSON.stringify(Products))
+}
